e9732fc9f49c9aaf388a438c3aa18296bab625bb
icebeam7 <luis.beltran@itcelaya.edu.mx>
2020-04-24 14:27:08 +0200

    Initial version

---
+++ AIEnrichmentSearchDemo/Views/AzureSearchView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace AIEnrichmentSearchDemo.Views
+{
+    public partial class AzureSearchView : ContentPage
+    {
+        public AzureSearchView()
+        {
+            InitializeComponent();
+        }
+    }
+}
